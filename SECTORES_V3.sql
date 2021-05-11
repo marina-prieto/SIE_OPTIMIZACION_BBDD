ba5403a4f8a2f91e40102464b63b689a58ede9c2
marina-prieto <marinaprieto999@gmail.com>
2021-05-12 01:00:41 +0200

    Create SECTORES_V3.sql

---
+++ SECTORES_V3.sql
@@ -0,0 +1,21 @@
+USE [SNTBROKER_SIE_MEJORADA]
+GO
+
+/****** Object:  Table [dbo].[SECTORES_v2]    Script Date: 22/02/2021 17:57:16 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[SECTORES_v3](
+	[ID_SECTOR] [int] NOT NULL,
+	[SECTOR] [nvarchar](40) NOT NULL
+) ON [PRIMARY]
+GO
+
+insert into [dbo].[SECTORES_v3]
+select
+	convert(int,IdSector) as ID_SECTOR,
+	convert(nvarchar(40),Sector) as SECTOR
+from [SNTBROKER_SIE].[dbo].[SECTORES_v2];
