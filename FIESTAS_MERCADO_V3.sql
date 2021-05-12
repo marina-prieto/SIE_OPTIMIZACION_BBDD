5f7dcac22c87e0799c8987eefffbdb0e4e110115
marina-prieto <marinaprieto999@gmail.com>
2021-05-12 11:12:35 +0200

    Update FIESTAS_MERCADO_V3.sql

--- FIESTAS_MERCADO_V3.sql
+++ FIESTAS_MERCADO_V3.sql
@@ -9,13 +9,13 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[FIESTAS_MERCADO_V3](
-	[ID_MERCADO] [varchar](6) NOT NULL,
+	[ID_MERCADO] int NOT NULL,
 	[FECHA] [smalldatetime] NOT NULL
 ) ON [PRIMARY]
 GO
 
 insert into [dbo].[FIESTAS_MERCADO_V3]
 select
-	convert(varchar(6),IdMercado) as ID_MERCADO,
+	convert(int,IdMercado) as ID_MERCADO,
 	convert(smalldatetime,Fecha) as FECHA
 from [SNTBROKER_SIE].[dbo].[FiestasMercados_v2]
